1e5c0f7b10361f9792fe9c28e432bebd12e8a79a
巴斯光年 <383514580@qq.com>
2016-05-01 23:19:13 +0800

    789

--- http.js
+++ http.js
@@ -1,13 +1,16 @@
 var express = require('express');
 var app = express();
+var open = require('open');
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+app.get('/hello', function(req, res) {
+    res.send('Hello World!');
 });
 
-var server = app.listen(3000, function () {
-  var host = server.address().address;
-  var port = server.address().port;
+app.use(express.static('public'));
 
-  console.log('Example app listening at http://%s:%s', host, port);
+var server = app.listen(3000, function() {
+    var host = server.address().address;
+    var port = server.address().port;
+    open('http://127.0.0.1:3000/hello');
+    // console.log('Example app listening at http://%s:%s', host, port);
 });
